ea6f72b94fdb7f5167cb32020f60d76bffdce69d
Adam Koewler <xadamxk@gmail.com>
2022-03-28 18:04:09 -0500

    test: update utilities unit tests

--- test/utitlities/index.test.ts
+++ test/utitlities/index.test.ts
@@ -1,13 +1,15 @@
-import { toInt } from '../src/utilities'
+import { toInt } from '../../src/utilities'
 
-test('valid int string to integer', () => {
-  expect(toInt('123')).toBe(123)
-})
+describe('toInt utility', () => {
+  test('should parse valid int-string to integer', () => {
+    expect(toInt('123')).toBe(123)
+  })
 
-test('valid binary string to integer', () => {
-  expect(toInt('1011', 2)).toBe(11)
-})
+  test('should parse valid binary-string to integer', () => {
+    expect(toInt('1011', 2)).toBe(11)
+  })
 
-test('invalid int string to default integer', () => {
-  expect(toInt('abc')).toBe(0)
+  test('should parse invalid int-string to default integer', () => {
+    expect(toInt('abc')).toBe(0)
+  })
 })
