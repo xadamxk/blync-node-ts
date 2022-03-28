3d3b6fcdb3d1490da380846f9abe8c0ec725fe91
Adam Koewler <xadamxk@gmail.com>
2022-03-28 18:05:53 -0500

    fix: toInt parse logic

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,7 +1,6 @@
 export const toInt = (value: string | number, base = 10): number => {
   try {
     const valueStr = value.toString()
-    if (isNaN(Number(valueStr))) throw Error('Not a number')
     return parseInt(valueStr, base)
   } catch (err) {
     return 0
