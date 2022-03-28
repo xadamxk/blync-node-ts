b82ec5fd5be9c55bddc8bb50049005f3add38856
Adam Koewler <xadamxk@gmail.com>
2022-03-28 18:15:35 -0500

    bugfix: fix toInt logic for results that arent a valid number

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,7 +1,11 @@
 export const toInt = (value: string | number, base = 10): number => {
   try {
     const valueStr = value.toString()
-    return parseInt(valueStr, base)
+    const intValue = parseInt(valueStr, base)
+    if (isNaN(intValue)) {
+      throw Error('')
+    }
+    return intValue
   } catch (err) {
     return 0
   }
