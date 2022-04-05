d41b25485dca6039391499e88b0f8d6a056c697b
Adam Koewler <xadamxk@gmail.com>
2022-04-04 19:45:30 -0500

    feat: update product list

--- src/classes/BlyncConnector.ts
+++ src/classes/BlyncConnector.ts
@@ -7,11 +7,20 @@ export class BlyncConnector {
   // Blync Light info obtained from DeviceAccess.cs
   blyncProducts: BlyncProduct[] = [
     // eslint-disable-next-line prettier/prettier
-    new BlyncProduct('2c0d', '0009', BlyncLightProductsEnum.BLYNCLIGHT_EMBRAVA_EMBEDDED, 12),
-    new BlyncProduct('0e53', '2519', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 7),
-    new BlyncProduct('2c0d', '0003', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 7),
+    new BlyncProduct('2c0d', '0009', BlyncLightProductsEnum.BLYNCLIGHT_EMBRAVA_EMBEDDED, 12), // ?
+    new BlyncProduct('0e53', '2519', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 7), // BLYNCLIGHT_MINI
+    new BlyncProduct('2c0d', '0003', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 7), // BLYNCLIGHT_MINI
     // eslint-disable-next-line prettier/prettier
-    new BlyncProduct('2c0d', '000a', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 13),
+    new BlyncProduct('2c0d', '000a', BlyncLightProductsEnum.BLYNCLIGHT_MINI, 13), // BLYNCLIGHT_MINI
+    new BlyncProduct('1130', '0001', BlyncLightProductsEnum.BLYNCLIGHT, 1), // BLYNCUSB10 DEVICE
+    new BlyncProduct('1130', '0002', BlyncLightProductsEnum.BLYNCLIGHT, 1), // BLYNCUSB10 DEVICE
+    new BlyncProduct('0e53', '2516', BlyncLightProductsEnum.BLYNCLIGHT, 3), // BLYNCUSB30 DEVICE
+    new BlyncProduct('2c0d', '0001', BlyncLightProductsEnum.BLYNCLIGHT, 3), // BLYNCUSB30 DEVICE
+    new BlyncProduct('0e53', '2517', BlyncLightProductsEnum.BLYNCLIGHT, 4), // BLYNCUSB30S DEVICE
+    new BlyncProduct('2c0d', '0002', BlyncLightProductsEnum.BLYNCLIGHT, 4), // BLYNCUSB30S DEVICE
+    new BlyncProduct('2c0d', '000c', BlyncLightProductsEnum.BLYNCLIGHT_IPE, 15), // BLYNCUSB40 DEVICE
+    // eslint-disable-next-line prettier/prettier
+    new BlyncProduct('2c0d', '0010', BlyncLightProductsEnum.BLYNCLIGHT_PLUS, 16), // BLYNCUSB40S DEVICE
   ]
 
   private indexCheck = (index: number, devices: BlyncDevice[]) => {
